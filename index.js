2f2679a69fc1cfb8a2d6c38ebf5ef740e48ea1bc
Alexandra Fren <alexandra.fren@gmail.com>
2019-01-02 17:46:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,6 +6,5 @@ function preventRefreshOnSubmit(){
 
 preventRefreshOnSubmit()
 
-function retrieveEmployeeInformation(input_value){
-  return document.getElementsByClassName('input_value')[0].value;
+function retrieveEmployeeInformation(){
 }
