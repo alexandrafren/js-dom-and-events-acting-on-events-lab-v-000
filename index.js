b0b1f179250c6e244ac4be3eb4766514ea833385
Alexandra Fren <alexandra.fren@gmail.com>
2019-01-02 18:04:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,5 +13,5 @@ function retrieveEmployeeInformation(){
 function addNewElementAsLi(){
   var empname = document.createElement('LI');
   empname.innerHTML = retrieveEmployeeInformation;
-  document.getElementByClass("employee-list").appendChild(empname);
+  document.getElementByClassName("employee-list").appendChild(empname);
 }
